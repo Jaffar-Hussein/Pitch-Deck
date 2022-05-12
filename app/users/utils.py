9ad26b26c7e2b03be6f1f002d21ea83097f3b4a2
Hanan-Hussein <hanhuss035@gmail.com>
2022-05-12 07:54:28 +0300

    blueprints-added blueprints to classes

---
+++ app/users/utils.py
@@ -0,0 +1,26 @@
+import secrets
+import os
+from PIL import Image
+
+def save_picture(form_picture):
+    """
+    
+    """
+
+    #base of file name
+    random_hex= secrets.token_hex(8)
+    _,f_ext=os.path.splitext(form_picture.filename)
+    picture_fn=random_hex +f_ext
+
+    picture_path=os.path.join(app.root_path,'static/profiles',picture_fn)
+
+    output_size=(125,125)
+    i=Image.open(form_picture)
+    i.thumbnail(output_size)
+
+    i.save(picture_path)
+    return picture_fn
+def comments_cutter(string_comments):
+    """Takes in a string cuts it and returns a list of comments
+    """
+    return string_comments.split(';')
